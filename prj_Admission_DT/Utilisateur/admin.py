3bc447012db02b65f92fd6196a161a101b8c1e79
Kassambara <ak.70810340@gmail.com>
2023-11-20 04:33:57 -0500

    Voila le SPRINT 1 et 2 Pour le Projet De La Demande Admission

---
+++ prj_Admission_DT/Utilisateur/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Utilisateurs,Trimestre,Programme
+# Register your models here.
+admin.site.register(Utilisateurs)
+admin.site.register(Trimestre)
+admin.site.register(Programme)
