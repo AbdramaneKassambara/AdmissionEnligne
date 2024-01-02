3aece5b9c84aebba8d08fe0192dddffd6829da2a
Abdramane Kassambara <141866030+AbdramaneKassambara@users.noreply.github.com>
2024-01-02 04:39:46 -0500

    Delete prj_Admission_DT/static directory

--- prj_Admission_DT/static/js/accuiel.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-  const welcomeMessage = document.querySelector(".student-info h2");
-  welcomeMessage.addEventListener("click", function () {
-    alert("Bienvenue sur votre page d'accueil !");
-  });
-});
