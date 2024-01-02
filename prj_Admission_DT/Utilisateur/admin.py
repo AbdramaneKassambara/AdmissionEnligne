bf60cec418962450bcd097126ae6a3d88fdc965b
Abdramane Kassambara <141866030+AbdramaneKassambara@users.noreply.github.com>
2024-01-02 04:39:34 -0500

    Delete prj_Admission_DT/Utilisateur directory

--- prj_Admission_DT/Utilisateur/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Utilisateurs,Trimestre,Programme
-# Register your models here.
-admin.site.register(Utilisateurs)
-admin.site.register(Trimestre)
-admin.site.register(Programme)
